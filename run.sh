128eea6e16da29d46dade2836a9a59fe38957fd2
Christoph Neumann <christophneumann2005@gmail.com>
2021-04-23 07:22:32 +0200

    Added a script to run the program

---
+++ run.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+#Runs the program, if it is located in the same directory. This is needed to set the LD_LIBRARY_PATH.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+LD_LIBRARY_PATH="$DIR/Lib" $DIR/GL_Proj $DIR
