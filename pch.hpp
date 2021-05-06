e629e9e3aab40598217699b8ce46dc871bf232fd
Christoph Neumann <christophneumann2005@gmail.com>
2021-05-06 17:38:08 +0200

    Updated pch with matrix translate and removed duplicate include

--- pch.hpp
+++ pch.hpp
@@ -1,9 +1,9 @@
 #pragma once
 #include <glad/glad.h>
 #include <string>
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glad/glad.h>
 #include <cmath>
 #include <algorithm>
 #include <functional>
