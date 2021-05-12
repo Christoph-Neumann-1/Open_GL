d13d6a0b0659ed821eba45c1599b9d920625e1b4
Christoph Neumann <christophneumann2005@gmail.com>
2021-05-12 18:58:47 +0200

    tmp

--- include/Voxel/Chunk.hpp
+++ include/Voxel/Chunk.hpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <glm/glm.hpp>
+#include <glad/glad.h>
 
 #define C_AT(x, y, z) x * 16 * 64 + y * 64 + z
 
@@ -12,7 +13,9 @@ namespace GL::Voxel
     class Chunk
     {
         int posx, posy;
+        uint va, texbuf, posbuf;
         u_char *blocks;
+        float *b_float;
         glm::vec3 *positions;
 
         void SetupPos()
@@ -28,6 +31,33 @@ namespace GL::Voxel
         {
             positions = reinterpret_cast<glm::vec3 *>(malloc(16 * 16 * 64 * sizeof(glm::vec3)));
             blocks = reinterpret_cast<u_char *>(malloc(16 * 16 * 64));
+            b_float = reinterpret_cast<float *>(malloc(16 * 16 * 64 * 4));
+            for (int i = 0; i < 16 * 16 * 64; i++)
+                b_float[i] = blocks[i];
+            glGenVertexArrays(1, &va);
+            glGenBuffers(2, &texbuf);
+            glBindVertexArray(va);
+
+            glBindBuffer(GL_ARRAY_BUFFER, vb);
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, 5 * sizeof(float), 0);
+            glVertexAttribPointer(1, 2, GL_FLOAT, false, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+            glEnableVertexAttribArray(0);
+            glEnableVertexAttribArray(1);
+
+            glBindBuffer(GL_ARRAY_BUFFER, posbuf);
+            glBufferData(GL_ARRAY_BUFFER, 16 * 16 * 64 * 3 * sizeof(float), b_float, GL_STATIC_DRAW);
+            glVertexAttribPointer(2, 3, GL_FLOAT, false, 3 * sizeof(float), 0);
+            glEnableVertexAttribArray(2);
+            glVertexAttribDivisor(2, 1);
+
+            glBindBuffer(GL_ARRAY_BUFFER, texbuf);
+            glBufferData(GL_ARRAY_BUFFER, 16 * 16 * 64 * sizeof(float), b_float, GL_STATIC_DRAW);
+            glVertexAttribPointer(3, 1, GL_FLOAT, false, sizeof(float), 0);
+            glEnableVertexAttribArray(3);
+            glVertexAttribDivisor(3, 1);
+
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindVertexArray(0);
         }
 
         void Load(int x, int y)
