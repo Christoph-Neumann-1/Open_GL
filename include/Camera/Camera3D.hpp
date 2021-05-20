0cf6c1ea4daa05dacfea0ab1f95f9c4d33325c3a
Christoph Neumann <christophneumann2005@gmail.com>
2021-05-20 20:44:07 +0200

    made camera functions const

--- include/Camera/Camera3D.hpp
+++ include/Camera/Camera3D.hpp
@@ -17,14 +17,14 @@ namespace GL
 
         Camera3D(glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f), glm::quat rot = glm::quat(1, 0, 0, 0)) : rotation(rot), position(pos) {}
 
-        glm::vec3 Up()
+        glm::vec3 Up() const
         {
             return glm::vec3(glm::vec4{0, 1, 0, 0} * glm::toMat4(rotation));
         }
-        glm::vec3 Forward() { return glm::vec3(glm::vec4{0, 0, -1, 0} * glm::toMat4(rotation)); }
-        glm::vec3 Right() { return glm::cross(Forward(), Up()); }
+        glm::vec3 Forward() const { return glm::vec3(glm::vec4{0, 0, -1, 0} * glm::toMat4(rotation)); }
+        glm::vec3 Right() const { return glm::cross(Forward(), Up()); }
 
-        glm::mat4 ComputeMatrix()
+        glm::mat4 ComputeMatrix() const
         {
             return glm::lookAt(
                 position,
