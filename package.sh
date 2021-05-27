dc766ad566b5e2830e739ce05b27cd7fdf821ee0
Christoph Neumann <christophneumann2005@gmail.com>
2021-05-27 15:54:59 +0200

    Auto crate tarball

--- package.sh
+++ package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf pkg
+
 mkdir -p pkg/scenes/bin pkg/modules/bin
 
 cp build/Opengl shader Lib README.md run.sh pkg -r
@@ -9,3 +11,5 @@ rsync -r res/ pkg/res/ --exclude res/world
 cp scenes/bin/* pkg/scenes/bin -r
 
 cp modules/bin/* pkg/modules/bin -r
+
+tar cf - pkg | xz -6 > pkg.tar.xz
