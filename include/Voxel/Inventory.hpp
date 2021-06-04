24022be7a5d699d47bc1cfe4e8a4f4a6e893bb67
Christoph Neumann <christophneumann2005@gmail.com>
2021-06-04 17:40:06 +0200

    Made inventory a state machine

--- include/Voxel/Inventory.hpp
+++ include/Voxel/Inventory.hpp
@@ -1,11 +1,34 @@
 #pragma once
-#include <Voxel/ConfigReader.hpp>
+#include <Voxel/Block.hpp>
+#include <array>
 
 namespace GL::Voxel
 {
     class Inventory
     {
-        TexConfig &blocks;
-        
+        std::array<int, 6> blocks;
+        uint block;
+
+        void Add()
+        {
+            blocks[block]++;
+        }
+        bool Remove()
+        {
+            if (blocks[block] <= 0)
+                return false;
+
+            blocks[block]--;
+            return true;
+        }
+        void Select(BlockTypes type)
+        {
+            block = type - 1;
+        }
+        uint GetCount()
+        {
+            return blocks[block];
+        }
+        BlockTypes GetSelected() { return (BlockTypes)(block + 1); }
     };
 }
