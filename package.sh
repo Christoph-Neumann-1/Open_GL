54dcec456d9ed3a9f3d12e86b5bdb36e576f13cf
Christoph Neumann <christophneumann2005@gmail.com>
2021-06-04 20:22:57 +0200

    Auto sign binary package

--- package.sh
+++ package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf pkg
+rm pkg.tar.xz pkg.tar.xz.sig
 
 mkdir -p pkg/scenes/bin pkg/modules/bin pkg/res
 
@@ -13,3 +14,6 @@ cp scenes/bin/* pkg/scenes/bin -r
 cp modules/bin/* pkg/modules/bin -r
 
 tar cf - pkg | xz -6 > pkg.tar.xz
+
+echo Please touch key
+gpg --detach-sign pkg.tar.xz 
