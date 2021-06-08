46c34cdce1e662b5add228266b74affcfd6a1199
Christoph Neumann <christophneumann2005@gmail.com>
2021-06-08 18:29:37 +0200

    fixed run and package script

--- package.sh
+++ package.sh
@@ -12,7 +12,7 @@ rsync -r res/ pkg/res/ --exclude={'res/world','*.kra','*~'}
 cp scenes/bin/* pkg/scenes/bin -r
 
 cp modules/bin/* pkg/modules/bin -r
-if [ $# -gt "0" ]
+if [ $# -gt 0 ]
 then
 if [[ $1 == "-noarchive" ]]
 then
@@ -22,7 +22,7 @@ fi
 
 tar cf - pkg | xz -6 > pkg.tar.xz
 
-if [ $# == "0" ] && [[ $1 != "-y" ]]
+if [ $# -gt 0 ] && [[ $1 != "-y" ]]
 then
 read -p "Sign Data ?" answer
 if [ "$answer" != "y" ] && [ "$answer" != "yes" ]
