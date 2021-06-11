0dd5992508685aeeae05bedb672b3baaadf50d63
Christoph Neumann <christophneumann2005@gmail.com>
2021-06-11 20:04:57 +0200

    Started laying out input system

--- include/Input.hpp
+++ include/Input.hpp
@@ -3,7 +3,6 @@
 #include <GLFW/glfw3.h>
 #include <Window.hpp>
 #include <unordered_map>
-#include <Callback.hpp>
 #include <Window.hpp>
 
 namespace GL
@@ -11,9 +10,32 @@ namespace GL
     ///@attention Only create one instance
     class InputHandler
     {
+        struct KeyCallbacks
+        {
+            struct Element
+            {
+                std::function<void()> func;
+                uint id;
+                bool operator==(uint idd)
+                {   
+                    return idd==id;
+                }
+
+                Element(const std::function<void()> &f, uint idd) : func(f), id(idd) {}
+                Element(std::function<void()> &&f, uint idd) : func(std::move(f)), id(idd) {}
+            };
+            std::vector<Element> funcs;
+            void Call()
+            {
+                for (auto &f : funcs)
+                    f.func();
+            }
+        };
+
         GL::Window &window;
-        std::unordered_map<int, CallbackList *> callbacks;
-        std::function<void(GLFWwindow *, int, int code, int action, int)> cbf;
+        std::unordered_map<int, KeyCallbacks *> callbacks;
+        std::mutex mutex;
+        uint current_id = 1;
 
         void CallbackFunc(int code, int action)
         {
@@ -23,12 +45,56 @@ namespace GL
         }
 
     public:
+        template <typename F, typename... Args>
+        class KeyCallback
+        {
+            uint id;
+            InputHandler &handle;
+            int scancode;
+
+            KeyCallback(InputHandler &handler, int code, F &&func, Args... args) : handle(handler), scancode(code)
+            {
+                id = handle.AddCallback(scancode, std::move(func), args...);
+            }
+            KeyCallback(InputHandler &handler, int code, const F &func, Args... args) : handle(handler), scancode(code)
+            {
+                id = handle.AddCallback(scancode, func, args...);
+            }
+            ~KeyCallback()
+            {
+                handle.RemoveCallback(scancode, id);
+            }
+        };
+
+        template <typename F, typename... Args>
+        uint AddCallback(int code, const F &func, Args... args)
+        {
+            std::scoped_lock mmutex(mutex);
+            callbacks[code]->funcs.emplace_back(std::bind(func, args...), current_id);
+            return current_id++;
+        }
+
+        template <typename F, typename... Args>
+        uint AddCallback(int code, F &&func, Args... args)
+        {
+            std::scoped_lock mmutex(mutex);
+            callbacks[code]->funcs.emplace_back(std::bind(std::move(func), args...), current_id);
+            return current_id++;
+        }
+
+        void RemoveCallback(int code, uint id)
+        {
+            std::scoped_lock mmutex(mutex);
+            auto &cbl = callbacks[code];
+            cbl->funcs.erase(std::find(cbl->funcs.begin(), cbl->funcs.end(), id));
+        }
+
         InputHandler(Window &w) : window(w)
         {
 
-            glfwSetKeyCallback(w, [](GLFWwindow * ww, int, int code, int action, int)
-                               { ((Window*)glfwGetWindowUserPointer(ww))->inputptr->CallbackFunc(code, action); });
-                               window.inputptr=this;
+            glfwSetKeyCallback(w, [](GLFWwindow *ww, int, int code, int action, int)
+                               { ((Window *)glfwGetWindowUserPointer(ww))->inputptr->CallbackFunc(code, action); });
+            window.inputptr = this;
         }
         ~InputHandler()
         {
