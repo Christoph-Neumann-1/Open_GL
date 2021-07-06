cc7d3cdf15502ad757b34fbaef94669c3ac74331
Christoph Neumann <christophneumann2005@gmail.com>
2021-07-06 17:39:41 +0200

    Updated the docs for the input class

--- include/Input.hpp
+++ include/Input.hpp
@@ -8,7 +8,7 @@
 namespace GL
 {
     //TODO: allow for text input
-    ///@attention Only create one instance 
+    ///@attention Only create one instance per window.
     class InputHandler
     {
     public:
@@ -23,6 +23,7 @@ namespace GL
         };
 
     private:
+        //Contains a list of all callbacks for each mouse button.
         struct MouseCallbacks
         {
             struct Element
@@ -54,6 +55,7 @@ namespace GL
             }
         };
 
+        //A list of all callbacks for a key.
         struct KeyCallbacks
         {
             struct Element
@@ -92,6 +94,7 @@ namespace GL
         std::mutex mutex;
         uint current_id = 1;
 
+        //This function is called every time something happens with a key. It then decides if a callback should be called.
         void KeyCallbackFunc(int code, int action)
         {
             //TODO  allow removing while called
@@ -102,6 +105,7 @@ namespace GL
             }
         }
 
+        //Same for mouse buttons.
         void MouseCallbackFunc(int button, int action)
         {
             //TODO  allow removing while called
@@ -113,6 +117,14 @@ namespace GL
         }
 
     public:
+        /**
+         * @brief This class allows you to register one callback for one key. 
+         * 
+         * While you can use the functions to add callbacks directly, I recommend this approach as
+         * it removes the risk of forgetting to remove the callback which might lead to segfaults.
+         * 
+         * You can still change the key later on or remove the callback early, so there is no real reason not to use this class.
+         */
         class KeyCallback
         {
             uint id = 0;
@@ -120,6 +132,18 @@ namespace GL
             int scancode;
 
         public:
+            /**
+             * @brief Bind a new function to a key.
+             * 
+             * If a callback was bound before it will be removed.
+             * The last parameter to the callback must be an integer. The integer is from glfw and represents the type of action.
+             * @tparam F any function or lambda
+             * @tparam arguments that need to be bound
+             * @param code key scancode
+             * @param type when the callback should be triggered
+             * @param func //The function that should be called
+             * @param args arguments that need to be bound
+             */
             template <typename F, typename... Args>
             void Bind(int code, Action type, F &&func, Args... args)
             {
@@ -128,6 +152,7 @@ namespace GL
                 id = handle.AddKeyCallback(scancode, type, std::move(func), args...);
             }
 
+            ///@overload
             template <typename F, typename... Args>
             void Bind(int code, Action type, const F &func, Args... args)
             {
@@ -136,31 +161,52 @@ namespace GL
                 id = handle.AddKeyCallback(scancode, type, func, args...);
             }
 
+            ///@brief Disables the callback.
             void UnBind()
             {
                 if (id == 0)
                     return;
                 handle.RemoveKeyCallback(scancode, id);
             }
+
+            /**
+             * @brief Construct a new Key Callback object but don't bind it to a key.
+             * 
+             * @param handler the input handler of the window
+             */
             KeyCallback(InputHandler &handler) : handle(handler) {}
 
+            /**
+             * @brief Construct a new Key Callback object and bind a callback directly
+             * 
+             * For more information see Bind()
+             */
             template <typename F, typename... Args>
             KeyCallback(InputHandler &handler, int code, Action type, F &&func, Args... args) : handle(handler)
             {
                 Bind(code, type, std::move(func), args...);
             }
 
+            ///@overload
             template <typename F, typename... Args>
             KeyCallback(InputHandler &handler, int code, Action type, const F &func, Args... args) : handle(handler)
             {
                 Bind(code, type, func, args...);
             }
+
+            ///Simply unbinds any bound callback.
             ~KeyCallback()
             {
                 UnBind();
             }
         };
 
+        /**
+         * @brief This class records the current state of the key.
+         * 
+         * Its main use is if you want to access the state of a key from a different thread.
+         * 
+         */
         class KeyState
         {
             uint id;
@@ -179,9 +225,17 @@ namespace GL
             {
                 handle.RemoveKeyCallback(scancode, id);
             }
+
+            ///@brief Returns the state of the key.
             const int &GetValue() { return value; }
         };
 
+        /**
+         * @brief Look at KeyCallback for more information.
+         * 
+         * Its almost the same just replace the scancode with something like GLFW_MOUSE_BUTTON_LEFT.
+         * 
+         */
         class MouseCallback
         {
             uint id = 0;
@@ -229,6 +283,11 @@ namespace GL
                 UnBind();
             }
         };
+
+        //The following functions add or remove callbacks from the lists. They all require the key or mouse button as well as an id.
+        //The id is used to identify a callback. It is represented as a uint and there is only one counter for all callbacks.
+        //If you register more callbacks than an uint can hold, you are doing something wrong.
+        //All callbacks need an int as the last parameter. The int comes directly from glfw and represents the press or release.
 
         template <typename F, typename... Args>
         uint AddKeyCallback(int code, Action type, const F &func, Args... args)
@@ -280,21 +339,30 @@ namespace GL
             cbl.funcs.erase(std::find(cbl.funcs.begin(), cbl.funcs.end(), id));
         }
 
+        /** @brief This will bind the glfw key callback functions as well as set a value in the window class. 
+         * 
+         * That value is neccesary, because glfw only allows setting one user pointer so some class needs to store all 
+         * information needed in callback funtions. It made sense to use the window class for that as it already used this user pointer.
+         **/
         InputHandler(Window &w) : window(w)
         {
 
             glfwSetKeyCallback(w, [](GLFWwindow *ww, int, int code, int action, int)
                                { ((Window *)glfwGetWindowUserPointer(ww))
                                      ->inputptr->KeyCallbackFunc(code, action); });
-            glfwSetMouseButtonCallback(w,[](GLFWwindow *ww, int button, int action, int)
-                               { ((Window *)glfwGetWindowUserPointer(ww))
-                                     ->inputptr->MouseCallbackFunc(button, action); });
+            glfwSetMouseButtonCallback(w, [](GLFWwindow *ww, int button, int action, int)
+                                       { ((Window *)glfwGetWindowUserPointer(ww))
+                                             ->inputptr->MouseCallbackFunc(button, action); });
             //TODO add mouse position listener
             window.inputptr = this;
         }
+
+        ///@brief This will remove the glfw callbacks. The user pointer is also set to null.
         ~InputHandler()
         {
             glfwSetKeyCallback(window, nullptr);
+            glfwSetMouseButtonCallback(window, nullptr);
+            window.inputptr = nullptr;
         }
     };
 }
