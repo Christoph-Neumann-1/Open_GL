3a294fe7b89c3b3b81908e4c6de33339ce6001e7
Christoph Neumann <christophneumann2005@gmail.com>
2021-07-10 10:42:21 +0200

    Added Lib Folder to RUN_PATH and removed run script

--- run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#Runs the program, if it is located in the same directory. This is needed to set the LD_LIBRARY_PATH.
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-mkdir -p $DIR/res/world
-
-LD_LIBRARY_PATH="$DIR/Lib" $DIR/Opengl $DIR
