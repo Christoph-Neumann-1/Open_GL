7e59927773c94038b32ddf883a12ea42d53c8361
Christoph Neumann <christophneumann2005@gmail.com>
2021-07-13 14:17:41 +0200

    Allowed for interaction by changing average velocity

--- scenes/Gas.cpp
+++ scenes/Gas.cpp
@@ -6,16 +6,18 @@
 #include <Camera/Camera3D.hpp>
 #include <Camera/Fplocked.hpp>
 #include <random>
+#include <Input.hpp>
 
 using namespace GL;
 
-const uint natoms = 28;
+const uint natoms = 50;
 const float initial_velocity = 0.2f;
 const float dt_factor = 0.8f;
 const float physics_rate = 5000;
 
-const float attraction = 120;
+const float attraction = 110;
 const float equal_dist = 0.07f;
+const float G = 0.15f;
 
 class AtomsSim : public Scene
 {
@@ -23,9 +25,26 @@ class AtomsSim : public Scene
     Shader shader{ROOT_Directory + "/shader/Atoms.vs", ROOT_Directory + "/shader/Atoms.fs"};
     Shader wall_shader{ROOT_Directory + "/shader/Default.vs", ROOT_Directory + "/shader/Default.fs"};
 
+    std::mutex mutex;
+
     uint vb, va;
 
+    float Temperature;
+
+    glm::vec2 temp_gauge[6]{
+        glm::vec2(1.0f, 0.0f),
+        glm::vec2(1.0f, 1.0f),
+        glm::vec2(0.0f, 1.0f),
+        glm::vec2(0.0f, 1.0f),
+        glm::vec2(0.0f, 0.0f),
+        glm::vec2(1.0f, 0.0f)
+    };
+    
+    glm::mat4 ortho_ui=glm::ortho(0.0f, 100.0f, 0.0f, 1000.0f, -1.0f, 1.0f);
+
     uint offsets;
+
+
 
     struct Atom
     {
@@ -97,8 +116,7 @@ class AtomsSim : public Scene
         shader.Bind();
 
         shader.SetUniformMat4f("u_V", camera.ComputeMatrix());
-        shader.SetUniform3f("u_CameraPosition",camera.position);
-
+        shader.SetUniform3f("u_CameraPosition", camera.position);
 
         FillBuffer();
         atom_model.Draw(shader, natoms);
@@ -108,7 +126,7 @@ class AtomsSim : public Scene
     void CollideWithWalls()
     {
         for (uint i = 0; i < atoms.size(); i++)
-            for (int j = 0; j < 3; j++)
+            for (uint j = 0; j < 3; j++)
             {
                 if (atoms[i].pos[j] + bradius > 1 || atoms[i].pos[j] - bradius < -1)
                 {
@@ -134,13 +152,32 @@ class AtomsSim : public Scene
         }
     }
 
+    void ComputeGravity(float dt)
+    {
+        for (auto &atom : atoms)
+        {
+            atom.vel -= G * dt;
+        }
+    }
+
+    void UpdatePositions(float dt)
+    {
+        for (auto &atom : atoms)
+        {
+            atom.pos += atom.vel * dt;
+        }
+    }
+
     void Update()
     {
-        auto dt = loader->GetTimeInfo().UpdateInterval() * dt_factor;
-        CollideWithWalls();
-        ComputeForces(dt);
-        for (uint i = 0; i < natoms; i++)
-            atoms[i].pos += atoms[i].vel * dt;
+        static float dt = loader->GetTimeInfo().UpdateInterval() * dt_factor;
+        {
+            std::scoped_lock lock(mutex);
+            CollideWithWalls();
+            ComputeForces(dt);
+            ComputeGravity(dt);
+            UpdatePositions(dt);
+        }
     }
 
     //TODO: Avoid collisions
@@ -157,40 +194,56 @@ class AtomsSim : public Scene
         }
     }
 
+    InputHandler::KeyCallback pluskey{*loader->GetWindow().inputptr, glfwGetKeyScancode(GLFW_KEY_KP_ADD), InputHandler::Action::Press, &AtomsSim::ModifyVelocity, this, 1.1f};
+    InputHandler::KeyCallback minuskey{*loader->GetWindow().inputptr, glfwGetKeyScancode(GLFW_KEY_KP_SUBTRACT), InputHandler::Action::Press, &AtomsSim::ModifyVelocity, this, 0.9f};
+
+    void ModifyVelocity(float change_percent, int)
+    {
+        std::scoped_lock lock(mutex);
+
+        for (auto &atom : atoms)
+        {
+            atom.vel *= change_percent;
+        }
+    }
+
 public:
     AtomsSim(SceneLoader *_loader) : Scene(_loader)
     {
+#pragma region Buffers
         glGenVertexArrays(1, &va);
         glBindVertexArray(va);
         glGenBuffers(1, &vb);
+        glGenBuffers(1, &offsets);
         glBindBuffer(GL_ARRAY_BUFFER, vb);
+
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
         glBufferData(GL_ARRAY_BUFFER, sizeof(sides), &sides, GL_STATIC_DRAW);
 
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-        RegisterFunc(CallbackType::Render, &AtomsSim::Render, this);
-        RegisterFunc(CallbackType::Update, &AtomsSim::Update, this);
-
-        GetFlag("hide_menu") = true;
-
-        glGenBuffers(1, &offsets);
         glBindBuffer(GL_ARRAY_BUFFER, offsets);
 
-        atoms.reserve(natoms);
-        Setup();
         glBufferData(GL_ARRAY_BUFFER, natoms * sizeof(glm::vec3), nullptr, GL_DYNAMIC_DRAW);
-
         InstanceBufferLayout layout;
         layout.stride = sizeof(glm::vec3);
         layout.attributes.push_back({GL_FLOAT, 3, 0});
         atom_model.AddInstanceBuffer(layout, offsets);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+#pragma endregion
+
+        atoms.reserve(natoms);
+        Setup();
+
+        RegisterFunc(CallbackType::Render, &AtomsSim::Render, this);
+        RegisterFunc(CallbackType::Update, &AtomsSim::Update, this);
+
+        GetFlag("hide_menu") = true;
+
         loader->GetTimeInfo().SetUpdateInterval(1 / physics_rate);
 
+#pragma region Uniforms
         shader.Bind();
         shader.SetUniform3f("u_Color", b_color);
         shader.SetUniformMat4f("u_P", proj);
@@ -199,10 +252,12 @@ public:
         shader.SetUniform3f("u_LightDirection", glm::normalize(glm::vec3(0, -1, -0.3)));
         shader.SetUniform3f("u_LightColor", glm::vec3(0.32));
         shader.SetUniform1f("u_Ambient", 0.65);
-        shader.SetUniform1f("u_Specular",0.8f);
-        shader.SetUniform1f("u_Shininess",15.0f);
+        shader.SetUniform1f("u_Specular", 0.8f);
+        shader.SetUniform1f("u_Shininess", 15.0f);
 
         shader.UnBind();
+
+#pragma endregion
     }
     ~AtomsSim()
     {
